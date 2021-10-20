bf0ab823b78451d94c0501d186ecb612cc3155ac
Toyoaki Doi <kokorononakaniitumo@yahoo.co.jp>
2021-10-20 16:50:53 +0900

    add scan sqs list

---
+++ scripts/sqs/scanSqsList.js
@@ -0,0 +1,15 @@
+const AWS = require('aws-sdk');
+// Set the region
+AWS.config.update({ region: 'ap-northeast-1' });
+
+(async () => {
+  try {
+    const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+
+    const sqsList = await sqs.listQueues({}).promise();
+
+    console.log(sqsList);
+  } catch (e) {
+    console.error(e);
+  }
+})();
